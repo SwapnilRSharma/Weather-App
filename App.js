d049ce4ce13049ed6bea8fd8e69a0e7d4e952ed0
Swapnil Sharma <swapnil.sharma@xecurify.com>
2023-03-14 11:19:52 +0530

    Initial commit

--- App.js
+++ App.js
@@ -1,7 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
 
 export default function App() {
+
+  const [data, setData] = useState("")
+
+  const fetchWeatcherData = async () => {
+    const result  = await axios.get("https://api.weatherapi.com/v1/current.json?key=ec461047d8ff4ab6b1450343231403&q=London&aqi=yes")
+    console.log(result.data)
+  }
+
+  useEffect(() => {
+    fetchWeatcherData()
+  }, [])
+  
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
